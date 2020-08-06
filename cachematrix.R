9025ff7207cdd335593305cb912db37adb3e1aa8
Diperez90 <diperezgomez@gmail.com>
2020-08-06 16:15:15 -0500

    Se realizaron nuevos cambios para hallar la matriz inversa.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Se va a escribir una función en R que pueda almacenar
+## en caché los cálculos que consumen mucho tiempo.
 
-## Write a short comment describing this function
+## Esta función makecachematrix crea un vector especial 
+## para poder calcular la inversa de una matriz.
 
 makeCacheMatrix <- function(x = matrix()) {
+ inv <- NULL
+ set <- function(y) {
+         x <<- y
+         inv <<- NULL
+         }
+         get <- function() (x)
+         setInverse <- function(inverse) {inv <<- inverse}
+         getInverse <- function() {inv}
+         list(set = set, get = get, setInverse = setInverse, 
+              getInverse = getInverse)
+         }
 
-}
 
-
-## Write a short comment describing this function
+## Esta función calcula la inversa de una matriz. Aunque 
+## primero mira en la memoria cache si esta ya ha sido 
+## calculada; si no esta la calcula y la guarda en la 
+## memoria cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        inv <- x$getInverse()
+        
+        if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+        }
+                matr <- x$get()
+                inv <- solve(matr, ...)
+                x$setInverse(inv)
+                inv
+                }
+   
+## Vamos a correr el modelo planteado:
+
+#  source("cachematrix.R")
+
+## Luego vamos a tomar este ejemplo.
+
+ qmatrix <- makeCacheMatrix(matrix(6:9, 
+             nrow = 2, ncol = 2))
+ 
+ qmatrix$get()
+ 
+ qmatrix$getInverse()
+ 
+ cacheSolve(qmatrix)
+                
+
+
